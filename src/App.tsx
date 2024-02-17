4c79376696ef4d61c4e040f0d3a718d24b3c198c
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-02-17 17:04:09 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Provider from "@/components/provider/Provider";
+import Router from "@/router/Router";
+
+const App = () => {
+  return (
+    <Provider>
+      <Router />
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>dreamer</div>;
+};
+
+export default Home;
